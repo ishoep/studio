78d8833d49d03c971d20e15afd7b7393f6063ab3
shoel <safarovr171@gmail.com>
2024-11-30 15:52:12 +0500

    adaptation added

--- landing-page/script.js
+++ landing-page/script.js
@@ -11,3 +11,28 @@ $(document).ready(function(){
         } 
     });
 });
+
+
+
+// Получаем элементы кнопки открытия, закрытия, меню и ссылок
+const menuToggle = document.getElementById('menuToggle');
+const closeMenu = document.getElementById('closeMenu');
+const fullscreenMenu = document.getElementById('fullscreenMenu');
+const menuLinks = fullscreenMenu.querySelectorAll('a');
+
+// Открытие меню
+menuToggle.addEventListener('click', () => {
+    fullscreenMenu.classList.add('open');
+});
+
+// Закрытие меню по кнопке "✖"
+closeMenu.addEventListener('click', () => {
+    fullscreenMenu.classList.remove('open');
+});
+
+// Закрытие меню при клике на любую ссылку
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        fullscreenMenu.classList.remove('open');
+    });
+});
